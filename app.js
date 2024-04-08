1fd3c7993b9afc73ad3b1848c5ad64c4cb590fc3
jishnu <jishnu.dev.roy@gmail.com>
2024-04-08 21:52:47 +0530

    buggy update logic

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
-const express = require('express');
+const path = require("path");
 
-const feedRoutes = require('./routes/feed');
+const express = require("express");
 
-const bodyParser = require('body-parser');
-const mongoose  = require('mongoose');
-const MONGODB_URI = "mongodb+srv://clumpiness:r1fbR7A327xczldH@cluster0.qcwuzp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const feedRoutes = require("./routes/feed");
 
+const bodyParser = require("body-parser");
+
+const multer = require("multer");
+
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  "mongodb+srv://clumpiness:r1fbR7A327xczldH@cluster0.qcwuzp2.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-mongoose.connect(MONGODB_URI)
-    .then(result => {
-        app.listen('8080', () => console.log('server started at 8080'));
-    })
-    .catch(err => console.log(err));
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.status || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen("8080", () => console.log("server started at 8080"));
+  })
+  .catch((err) => console.log(err));
--- routes/feed.js
+++ routes/feed.js
@@ -6,12 +6,19 @@ const feedController = require('../controller/feed');
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPost);
+router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
 router.post('/post',[
-    body('title').trim().isLength({min: 7}),
+    body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ], feedController.createPost);
+
+router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', [
+    body('title').trim().isLength({min: 5}),
+    body('content').trim().isLength({min: 5})
+], feedController.updatePost);
 
 module.exports = router;
