f5232b186cf33f0bb61552ce02779d6b46d3d42c
jishnu <jishnu.dev.roy@gmail.com>
2024-04-11 18:36:59 +0530

    async auth js

--- controller/auth.js
+++ controller/auth.js
@@ -1,76 +1,71 @@
-const { validationResult } = require('express-validator');
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { validationResult } = require("express-validator");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+exports.signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    const error = new Error("Validation failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
 
-
-exports.signup = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty) {
-        const error = new Error('Validation failed');
-        error.statusCode = 422;
-        error.data = errors.array();
-        throw error;
+  try {
+    const hashedPw = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      name: name,
+    });
+    const result = await user.save();
+    res
+      .status(201)
+      .json({ message: "User created successfully!", userId: result.userId });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
     }
-    const email = req.body.email;
-    const name = req.body.name;
-    const password = req.body.password;
+    next(err);
+  }
+};
 
-    bcrypt.hash(password, 12)
-        .then(hashedPw => {
-            const user = new User({
-                email: email,
-                password: hashedPw,
-                name: name
-            });
-            return user.save();
-        })
-        .then( result => {
-            res.status(201).json({ message: 'User created successfully!', userId: result.userId })
-        })  
-        .catch((err) => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
-}
+exports.login = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  const user = await User.findOne({ email: email });
 
-exports.login = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    let loadedUser;
-    User.findOne({email: email})
-        .then(user => {
-            if (!user) {
-                const error = new Error('User not found.');
-                error.statusCode = 401;
-                throw error;
-            }
-            loadedUser = user;
-            return bcrypt.compare(password, user.password);
-        })
-        .then(isEqual => {
-            if (!isEqual) {
-                const error = new Error('Wrong password.');
-                error.statusCode = 401;
-                throw error;
-            }
-            const token = jwt.sign(
-                {
-                    email: email,
-                    userId: loadedUser._id.toString()
-                },
-                'somesupersecretkey',
-                { expiresIn: '1h' }
-            );
-            res.status(200).json({ token: token, userId: loadedUser._id.toString() })
-        })
-        .catch((err) => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
-}
+  try {
+    if (!user) {
+      const error = new Error("User not found.");
+      error.statusCode = 401;
+      throw error;
+    }
+    loadedUser = user;
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Wrong password.");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        email: email,
+        userId: loadedUser._id.toString(),
+      },
+      "somesupersecretkey",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
