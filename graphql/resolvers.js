31ba980785be44f5ff60b3648d8d1b39f336bd6e
jishnu <jishnu.dev.roy@gmail.com>
2024-04-19 14:25:29 +0530

    graphql signup done

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,36 @@
+const User = require('../models/user');
+
+const validator = require('validator');
+
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-    hello() {
-        return {
-            text: 'Hello World!',
-            view: 12345
-        };
+    createUser: async function({ userInput }, req) {
+        const errors = [];
+        if (!validator.isEmail(userInput.email)) {
+            errors.push({message: 'Invalid email.'});
+        }
+        if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min: 3})) {
+            errors.push({message: 'Invalid password.'});
+        }
+
+        if (errors.length > 0) {
+            throw new Error('Invalid input.');
+        }
+
+        const existingUser = await User.findOne({email: userInput.email});
+        if (existingUser) {
+            const err = new Error('User aleready exsists');
+            throw err;
+        }
+        const hashedPw = await bcrypt.hash(userInput.password, 12);
+        const user = new User({
+            email: userInput.email,
+            password: userInput.password,
+            name: userInput.name
+        });
+        const createdUser = await user.save();
+        return { ...createdUser._doc, _id: createdUser._id.toString() };
+
     }
 }
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,16 +1,41 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-    type TestData {
-        text: String!
-        views: Int! 
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type User {
+        _id: ID!
+        email: String!
+        name: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+
+    input userInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(userInput: userInputData): User!
     }
 
     type RootQuery {
-        hello: TestData
+        hello: String
     }
 
     schema {
         query: RootQuery
+        mutation: RootMutation
     }
 `);
