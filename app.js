e4c368c6b47691d4c8fed1a28276d0a65f6ab903
jishnu <jishnu.dev.roy@gmail.com>
2024-04-21 16:26:25 +0530

    file upload done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require('fs');
 
 const express = require("express");
 
@@ -64,6 +65,23 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated.');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided.' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path });
+});
+
+
+
 app.use('/graphql', graphqlHTTP({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
@@ -93,3 +111,8 @@ mongoose
     app.listen("8080", () => console.log("server started at 8080"));
   })
   .catch((err) => console.log(err));
+
+  const clearImage = (filePath) => {
+    filePath = path.join(__dirname, "..", filePath);
+    fs.unlink(filePath, (err) => console.log(err));
+  };
