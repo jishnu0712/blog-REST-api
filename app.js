46ef4e75555ade5527c2ab451ccc1de4ddb155ea
jishnu <jishnu.dev.roy@gmail.com>
2024-04-24 15:59:17 +0530

    port dynamic

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    const server = app.listen("8080", () => console.log("server started at 8080"));
+    const server = app.listen(process.env.PORT || "8080", () => console.log(`server started at ${process.env.PORT || 8080}`));
     const io = require('./socket').init(server);
     io.on('connection', socket => {
       console.log('client connected');
